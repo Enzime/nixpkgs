3f7ad17417c7b17cfc29d4ad19c5c18008b1dfb1
Michael Hoang <enzime@users.noreply.github.com>
2019-12-07 14:30:14 +1100

    linux-testing-drm-next: 2019-11-27 -> 2019-12-06

--- pkgs/os-specific/linux/kernel/linux-testing-drm-next.nix
+++ pkgs/os-specific/linux/kernel/linux-testing-drm-next.nix
@@ -3,13 +3,13 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.5-2019-11-27";
+  version = "5.5-2019-12-06";
   modDirVersion = "5.4.0-rc7";
 
   src = fetchgit {
     url = "https://anongit.freedesktop.org/git/drm/drm.git";
-    rev = "acc61b8929365e63a3e8c8c8913177795aa45594";
-    sha256 = "0zaxq57zz5jfyscfsq5gq0fx7mr2z9nqgyb2r0sydx33z7ddl4is";
+    rev = "9c1867d730a6e1dc23dd633392d102860578c047";
+    sha256 = "1r6pbpnw3x68rrqlvhmx85ppb2k4n2c9y7lgzg8xmxjf6yd7lajg";
   };
 
   extraMeta = {
