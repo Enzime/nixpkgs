9d9f3eb11738c500ebd21419dffedc2e74b3e030
Michael Hoang <enzime@users.noreply.github.com>
2026-04-08 23:01:00 +0200

    qalculate-gtk: fix build on macOS

--- pkgs/by-name/qa/qalculate-gtk/package.nix
+++ pkgs/by-name/qa/qalculate-gtk/package.nix
@@ -2,10 +2,12 @@
   lib,
   stdenv,
   fetchFromGitHub,
+  fetchpatch,
   autoreconfHook,
   pkg-config,
   libqalculate,
   gtk3,
+  gtk-mac-integration,
   curl,
   wrapGAppsHook3,
   desktopToDarwinBundle,
@@ -22,6 +24,15 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-JZfolSRLRLtv529f25lEPYOlz+y+EdRqKA0Y5d1dK3s=";
   };
 
+  patches = [
+    # https://github.com/Qalculate/qalculate-gtk/pull/705
+    (fetchpatch {
+      name = "fix-platform-macos-conditional.patch";
+      url = "https://github.com/Qalculate/qalculate-gtk/commit/f32ced1525c4cfc8e2c31b3f0d192371d1ffadc3.patch";
+      hash = "sha256-v9T8wWvqlI9l6BUszf/575qmoFvh88Cdj/m2XqV8Q4k=";
+    })
+  ];
+
   hardeningDisable = [ "format" ];
 
   nativeBuildInputs = [
@@ -34,7 +45,8 @@ stdenv.mkDerivation (finalAttrs: {
     libqalculate
     gtk3
     curl
-  ];
+  ]
+  ++ lib.optionals stdenv.hostPlatform.isDarwin [ gtk-mac-integration ];
   enableParallelBuilding = true;
 
   meta = {
